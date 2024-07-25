fdaf53f86179bec9b7f504c97c88adb436c6d0b1
Ricardo Flor <rcrdfj@gmail.com>
2024-07-24 20:13:11 -0500

    Project published

--- obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+632af81921374860426aada8072369b71ab638aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
